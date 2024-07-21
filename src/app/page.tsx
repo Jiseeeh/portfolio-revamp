6ca56471c034af58cc37113b98631dfbc57ba7f2
Jiseeeh <johncarlo.camara1@gmail.com>
2024-07-21 22:33:26 +0800

    feat: add projects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import { About } from "@/sections/About";
+import { About } from "@/components/sections/About";
+import { Projects } from "@/components/sections/Projects";
 
 export default function Home() {
   return (
     <>
       <About />
+      <Projects />
     </>
   );
 }
