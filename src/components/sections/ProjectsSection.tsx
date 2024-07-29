946094aa98c08646211391254ca697ed07fa8a4c
Jiseeeh <johncarlo.camara1@gmail.com>
2024-07-29 16:55:15 +0800

    feat: add gsap

--- src/components/sections/ProjectsSection.tsx
+++ src/components/sections/ProjectsSection.tsx
@@ -1,10 +1,16 @@
 "use client";
 
-import { useEffect, useRef } from "react";
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
+
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useRef } from "react";
 
 import { SectionIntro } from "./SectionIntro";
 import { Project } from "@/components/ui/Project";
 import { ProjectModel } from "@/models/ProjectModel";
+
+gsap.registerPlugin(ScrollTrigger);
 
 interface ProjectsSectionProps {}
 
@@ -53,106 +59,40 @@ const ProjectsSection: React.FC<ProjectsSectionProps> = ({}) => {
     }),
   ];
 
-  useEffect(() => {
-    const modifyProjectScrolling = (e: WheelEvent) => {
-      // console.log(`scrollingY ${e.deltaY}\nscrollingX ${e.deltaX}`);
-      const container = scrollContainer.current as unknown as HTMLElement;
+  useGSAP(
+    () => {
+      const projectSections = gsap.utils.toArray("#projects > section");
 
-      if (e.deltaY > 0) {
-        // means scrolling down
-        // console.log({
-        //   scrollLeft: Math.round(container.scrollLeft),
-        //   scrollWidth: container.scrollWidth - container.offsetWidth,
-        // });
-        if (
-          Math.round(container.scrollLeft) !==
-          container.scrollWidth - container.offsetWidth
-        ) {
-          e.preventDefault();
-        }
-        container.scrollLeft += e.deltaY;
-        return;
-      }
-
-      // only allow scrollX if the scrollContainer.scrollLeft = 0
-      if (e.deltaY < 0) {
-        // means scrolling up
-
-        if (container.scrollLeft !== 0) {
-          e.preventDefault();
-        }
-
-        container.scrollLeft += e.deltaY;
-
-        return;
-      }
-
-      // horiz scroll
-    };
-
-    const snapToCenter = () => {
-      const container = scrollContainer.current as unknown as HTMLElement;
-      const sections = container.querySelectorAll("section");
-
-      let closestSection = sections[0];
-      let closestDistance = Infinity;
-
-      sections.forEach((section) => {
-        const distance = Math.abs(
-          section.getBoundingClientRect().left -
-            container.getBoundingClientRect().left
-        );
-
-        if (distance < closestDistance) {
-          closestSection = section;
-          closestDistance = distance;
-        }
+      gsap.to(projectSections, {
+        xPercent: -100 * (projectSections.length - 1),
+        ease: "none",
+        scrollTrigger: {
+          markers: true,
+          start: "100p top",
+          trigger: scrollContainer.current,
+          pin: true,
+          scrub: 1,
+          snap: 1 / (projectSections.length - 1),
+          end: () =>
+            "+=" +
+            (scrollContainer.current as unknown as HTMLElement).offsetWidth,
+        },
       });
-
-      container.scrollTo({
-        left:
-          closestSection.offsetLeft -
-          container.offsetLeft +
-          (closestSection.offsetWidth - container.offsetWidth) / 2,
-        behavior: "smooth",
-      });
-    };
-
-    const observerCallBack = (entries: IntersectionObserverEntry[]) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          console.log("Adding event listener");
-
-          document.addEventListener("wheel", modifyProjectScrolling, {
-            passive: false,
-          });
-          document.addEventListener("wheel", snapToCenter, {
-            passive: true,
-          });
-        } else {
-          console.log("Removing event listener");
-          document.removeEventListener("wheel", modifyProjectScrolling);
-          document.removeEventListener("wheel", snapToCenter);
-        }
-      });
-    };
-
-    const observer = new IntersectionObserver(observerCallBack, {
-      root: null,
-      rootMargin: "0px",
-      threshold: 0.95,
-    });
-
-    observer.observe(document.querySelector("#projects") as HTMLElement);
-  }, []);
+    },
+    {
+      scope: scrollContainer,
+    }
+  );
 
   return (
-    <section className="min-h-[80vh] flex flex-col">
+    <section className="">
       <SectionIntro sectionNumber="01" sectionTitle="Projects" />
       <section
         id="projects"
         ref={scrollContainer}
-        className="min-h-full flex overflow-x-hidden snap-x snap-mandatory md:snap-none md:snap-normal"
+        // ? extra padding top to account for the fixed navbar and the start of gsap scroll
+        // ? width must be based on the number of sections
+        className={`h-screen pt-24 lg:pt-0  w-[${projects.length}00vw] flex flex-nowrap`}
       >
         {projects.map((project) => (
           <Project key={project.id} {...project} />
