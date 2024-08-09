90299a94e5b02d933058354d5d96ca40c89f25f7
Jiseeeh <johncarlo.camara1@gmail.com>
2024-08-09 17:48:35 +0800

    feat: add tech stack section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { AboutSection } from "@/components/sections/AboutSection";
 import { EducationSection } from "@/components/sections/EducationSection";
 import { ProjectsSection } from "@/components/sections/ProjectsSection";
 import { ExperienceSection } from "@/components/sections/ExperienceSection";
+import { TechStackSection } from "@/components/sections/TechStackSection";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <ProjectsSection />
       <EducationSection />
       {/* <ExperienceSection /> */}
+      <TechStackSection />
     </>
   );
 }
