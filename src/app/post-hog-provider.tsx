ac364338099c97cfd60e22a5ea804369b22cd95b
Jiseeeh <johncarlo.camara1@gmail.com>
2024-08-11 02:42:47 +0800

    feat: replace with magic string

--- src/app/post-hog-provider.tsx
+++ src/app/post-hog-provider.tsx
@@ -7,7 +7,7 @@ import { useEffect } from "react";
 export function PostHogProvider({ children }: { children: React.ReactNode }) {
   useEffect(() => {
     posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-      api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+      api_host: "/ingest",
       person_profiles: "always",
       capture_pageview: false,
       capture_pageleave: true,
