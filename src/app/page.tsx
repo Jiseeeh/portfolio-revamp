3a95719420b9ec7f6aade0b4bc0636bc7265f2bc
Jiseeeh <johncarlo.camara1@gmail.com>
2024-08-10 15:44:23 +0800

    feat: add screen loader

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,8 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+import { Navbar } from "@/components/navigation/Navbar";
 import { AboutSection } from "@/components/sections/AboutSection";
 import { EducationSection } from "@/components/sections/EducationSection";
 import { ProjectsSection } from "@/components/sections/ProjectsSection";
@@ -6,8 +11,37 @@ import { TechStackSection } from "@/components/sections/TechStackSection";
 import { ContactSection } from "@/components/sections/ContactSection";
 
 export default function Home() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const finishLoading = () => {
+      setIsLoading(false);
+    };
+
+    if (document.readyState === "complete") {
+      finishLoading();
+    } else {
+      window.addEventListener("load", finishLoading);
+      return () => window.removeEventListener("load", finishLoading);
+    }
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div className="fixed top-0 w-screen min-h-screen bg-white z-50">
+        <div className="flex flex-col space-y-5 justify-center items-center min-h-screen">
+          <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-8 border-black"></div>
+          <span className="font-bold uppercase animate-bounce">
+            Summoning the tech spirits...
+          </span>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <>
+      <Navbar />
       <AboutSection />
       <ProjectsSection />
       <EducationSection />
